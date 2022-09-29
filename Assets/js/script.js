2bd256c6a159c9b993ee00bc79ff368db9899b23
Michael Andrews <michael.andrews@pega.com>
2022-09-29 00:32:06 -0400

    change script.js, css, and html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,30 @@
-// global variables and arrays
-var time = questions.length * 15
-var currentQuestionIndex = 0
+const questions = [
+    {
+        questionText: "What does HTML stand for?",
+        options: ["1. Hyper Text Makeup Lines", "2. Hyper Text Multi Language", "3. Hyper Text Markup Language", "4. Hyper Tool Makeup Length"],
+        answer: "3. Hyper Text Markup Language",
+    },
+    {
+        questionText: "Javascript is an ________________ language?",
+        options: ["1. Object-Oriented", "2. Object-Based", "3. Procedural", "4. None of the above"],
+        answer: "1. Object-Oriented",
+    },
+    {
+        questionText: "Arrays in JavaScript can be used to store ______.",
+        options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above",
+    },
 
-// All required elements.
-var startButton = document.querySelector('start-btn button')
+    {
+        questionText: "Which of the following is not a type of loop in javascript?",
+        options: ["1. do/while", "2. when", "3. for", "4. while"],
+        answer: "2. when",
+    },
+    {
+        questionText:
+            "Which is a not a commonly used data type?",
+        options: ["1. alerts", "2. boolean", "3. number", "4. string"],
+        answer: "1. alerts",
+    },
+];
 
-// clears local storage of scores
-function clearStorage () {
-  localStorage.clear()
-}
-
-// event listeners
